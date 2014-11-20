3805cad40d63059ac1083bdd5e55917b7d99f477
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-20 20:38:11 +0000

    Adding exercise-26

---
+++ exercise-26.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	fmt.Println("v.X ->", v.X)
+
+	v.X = 4
+	fmt.Println("v.X ->", v.X)
+}
