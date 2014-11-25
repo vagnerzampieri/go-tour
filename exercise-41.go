3d53bdb6c4abf2bf3134957c4bcc131ca6ec33a2
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-25 22:11:12 +0000

    Update

--- exercise-41.go
+++ exercise-41.go
@@ -9,6 +9,7 @@ func WordCount(s string) map[string]int {
 	sliceString := strings.Fields(s)
 	num := len(sliceString)
 	mountMap := make(map[string]int)
+
 	for i := 0; i < num; i++ {
 		(mountMap[sliceString[i]])++
 	}
