0e0ba1afc67b6d5f08fdb4904b24f27de66da3fd
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-25 22:01:05 +0000

    Adding exercise-41

---
+++ exercise-41.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"code.google.com/p/go-tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	sliceString := strings.Fields(s)
+	num := len(sliceString)
+	mountMap := make(map[string]int)
+	for i := 0; i < num; i++ {
+		(mountMap[sliceString[i]])++
+	}
+
+	return mountMap
+}
+
+func main() {
+	wc.Test(WordCount)
+}
