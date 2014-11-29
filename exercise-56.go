ba78c0aa02042f5cb433f3070a79cd7c10ae1dbb
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-29 21:35:15 +0000

    Adding exercise-56

---
+++ exercise-56.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
+}
+
+func Sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, ErrNegativeSqrt(f)
+	}
+	return 0, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
