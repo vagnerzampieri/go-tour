e4b4c3b84b970cf34c75daeb112a80ae1c209271
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-10 11:24:33 +0000

    Adding flag to control args

--- examples/measurement_converter/main.go
+++ examples/measurement_converter/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+	"flag"
 )
 
+var (
+	value       *float64
+	measurement *string
+)
+
+func init() {
+	value = flag.Float64("value", 10.07, "Adding value to converter")
+	measurement = flag.String("measurement", "celsius or fahrenheit", "Adding conversor")
+	flag.Parse()
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Use: converter <value> <measurement>")
-		os.Exit(1)
-	}
+	//	if len(os.Args) < 3 {
+	//		fmt.Println("Use: converter <value> <measurement>")
+	//		os.Exit(1)
+	//	}
 }
