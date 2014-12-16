096b826be53f1bb95cc395309f86a0168b6d9e04
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-16 13:47:17 -0200

    Adding variables to get args

--- examples/measurement_converter/main.go
+++ examples/measurement_converter/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -16,8 +18,18 @@ func init() {
 }
 
 func main() {
-	//	if len(os.Args) < 3 {
-	//		fmt.Println("Use: converter <value> <measurement>")
-	//		os.Exit(1)
-	//	}
+	//fmt.Println("len(os.Args) ->", len(os.Args))
+	//fmt.Println("os.Args ->", os.Args)
+	//fmt.Println("os.Args[0] ->", os.Args[0])
+
+	if len(os.Args) < 3 {
+		fmt.Println("Use: converter <value> <measurement>")
+		os.Exit(1)
+	}
+
+	originMeasurement := os.Args[len(os.Args)-1]
+	originValue := os.Args[1 : len(os.Args)-1]
+
+	fmt.Println("originMeasurement := os.Args[len(os.Args)-1]", originMeasurement)
+	fmt.Println("originValue := os.Args[1 : len(os.Args)-1]", originValue)
 }
