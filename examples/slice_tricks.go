4c63a75ec7951e291198f9cf7a0f60b728c7a427
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-18 16:27:41 -0200

    Ident

--- examples/slice_tricks.go
+++ examples/slice_tricks.go
@@ -3,30 +3,30 @@ package main
 import "fmt"
 
 func main() {
-    a := []string{"a", "b", "c"}
-    b := []string{"e"}
-    
-    fmt.Println(a)
-    
-    a = append(a, "d")
-    fmt.Println(a)
-    
-    a = append(a, b[0])
-    fmt.Println(a)
-    
-    fmt.Println(a[1:3])
-    
-    fmt.Println(a[2])
-    
-    c := []string{}
-    d := 2
-    e := a[:d]
-    f := a[d+1:]
-    fmt.Println(e)
-    fmt.Println(f)
-    
-    c = append(c, e...)
-    c = append(c, f...)
-    
-    fmt.Println(c)
+	a := []string{"a", "b", "c"}
+	b := []string{"e"}
+
+	fmt.Println(a)
+
+	a = append(a, "d")
+	fmt.Println(a)
+
+	a = append(a, b[0])
+	fmt.Println(a)
+
+	fmt.Println(a[1:3])
+
+	fmt.Println(a[2])
+
+	c := []string{}
+	d := 2
+	e := a[:d]
+	f := a[d+1:]
+	fmt.Println(e)
+	fmt.Println(f)
+
+	c = append(c, e...)
+	c = append(c, f...)
+
+	fmt.Println(c)
 }
