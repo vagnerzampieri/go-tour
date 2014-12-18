892d6bdf187bf96606270596a8be5c0e56b92140
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-18 14:39:42 -0200

    Adding exception when no args

--- examples/quicksort/main.go
+++ examples/quicksort/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var numbers []string
@@ -15,6 +16,11 @@ func init() {
 func main() {
 	numbers = flag.Args()
 	lenNumbers := len(numbers)
+
+	if lenNumbers == 0 {
+		fmt.Println("Need numbers as arguments.\nEx.: go run main.go 1 2 3")
+		os.Exit(1)
+	}
 
 	fmt.Println("numbers", numbers)
 	fmt.Println("len numbers", lenNumbers)
