abe444e644373e567e63ac887df731067de99867
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-18 16:35:07 -0200

    Adding notififcations

--- examples/slice_tricks.go
+++ examples/slice_tricks.go
@@ -6,27 +6,27 @@ func main() {
 	a := []string{"a", "b", "c"}
 	b := []string{"e"}
 
-	fmt.Println(a)
+	fmt.Println("a ->", a)
 
 	a = append(a, "d")
-	fmt.Println(a)
+	fmt.Println("append(a, 'd') ->", a)
 
 	a = append(a, b[0])
-	fmt.Println(a)
+	fmt.Println("append(a, b[0]) ->", a)
 
-	fmt.Println(a[1:3])
+	fmt.Println("a[1:3] ->", a[1:3])
 
-	fmt.Println(a[2])
+	fmt.Println("a[2] ->", a[2])
 
 	c := []string{}
 	d := 2
 	e := a[:d]
 	f := a[d+1:]
-	fmt.Println(e)
-	fmt.Println(f)
+	fmt.Println("a[:d] ->", e)
+	fmt.Println("a[d+1:] ->", f)
 
 	c = append(c, e...)
 	c = append(c, f...)
 
-	fmt.Println(c)
+	fmt.Println("append(c, e...) append(c, f...) ->", c)
 }
