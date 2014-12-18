6ad80482c10642a9e990e7976b4acd979f98102f
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-18 14:43:19 -0200

    Adding func errors to refactoring 'errors'

--- examples/quicksort/main.go
+++ examples/quicksort/main.go
@@ -6,22 +6,29 @@ import (
 	"os"
 )
 
-var numbers []string
+var (
+	numbers    []string
+	lenNumbers int
+)
 
 func init() {
 	flag.Int("numbers", 10, "Add numbers to be sorted")
 	flag.Parse()
+
+	numbers = flag.Args()
+	lenNumbers = len(numbers)
+
+	errors()
 }
 
-func main() {
-	numbers = flag.Args()
-	lenNumbers := len(numbers)
-
+func errors() {
 	if lenNumbers == 0 {
 		fmt.Println("Need numbers as arguments.\nEx.: go run main.go 1 2 3")
 		os.Exit(1)
 	}
+}
 
+func main() {
 	fmt.Println("numbers", numbers)
 	fmt.Println("len numbers", lenNumbers)
 	fmt.Println("numbers[0]", numbers[0])
