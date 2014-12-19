d40c0a0c3850efec65183f46e8837b7d8483dcae
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-19 15:36:46 -0200

    Add values in stack

--- examples/stack/main.go
+++ examples/stack/main.go
@@ -8,6 +8,13 @@ func main() {
 	stack := Stack{}
 	fmt.Println("Create stack with length", stack.Length())
 	fmt.Println("Empty?", stack.Empty())
+
+	stack.Add("Go")
+	stack.Add(2009)
+	stack.Add(3.14)
+	stack.Add("End")
+	fmt.Println("Length after 4 values:", stack.Length())
+	fmt.Println("Empty?", stack.Empty())
 }
 
 type Stack struct {
@@ -20,4 +27,8 @@ func (stack Stack) Length() int {
 
 func (stack Stack) Empty() bool {
 	return stack.Length() == 0
+}
+
+func (stack *Stack) Add(value interface{}) {
+	stack.values = append(stack.values, value)
 }
