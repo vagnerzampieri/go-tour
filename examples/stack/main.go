dd421c09e9d63a185e6a80daa3ab4b8fbc20a625
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-19 15:28:49 -0200

    Check stack length

--- examples/stack/main.go
+++ examples/stack/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
+	stack := Stack{}
+	fmt.Println("Create stack with length", stack.Length())
+}
 
+type Stack struct {
+	values []interface{}
+}
+
+func (stack Stack) Length() int {
+	return len(stack.values)
 }
