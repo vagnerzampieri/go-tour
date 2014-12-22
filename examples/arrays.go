dda7382b947d4fbc86ecb526219ddd638a5dea0a
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-22 13:25:11 +0000

    Adding array multidimension

--- examples/arrays.go
+++ examples/arrays.go
@@ -17,4 +17,13 @@ func main() {
 	// 0.0 para floats
 	// ""(ou string vazia) para strings
 	// nil para ponteiros, funcoes, interfaces, slices, maps e channels
+
+	var multiA [2][2]int
+	multiA[0][0], multiA[0][1] = 3, 5
+	multiA[1][0], multiA[1][1] = 7, -2
+
+	multiB := [2][2]int{{2, 13}, {-1, 6}}
+
+	fmt.Println("Multi A:", multiA)
+	fmt.Println("Multi B:", multiB)
 }
