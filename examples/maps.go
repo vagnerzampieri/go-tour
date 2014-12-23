b4bf8a64262791baa77e479a9fe38e5a17aa539a
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-23 12:33:24 +0000

    Print capitals

--- examples/maps.go
+++ examples/maps.go
@@ -8,4 +8,18 @@ func main() {
 
 	fmt.Println("empty1 ->", empty1)
 	fmt.Println("empty2 ->", empty2)
+
+	bigMap := make(map[int]string, 4096)
+
+	fmt.Println("len bigMap ->", len(bigMap))
+
+	capitals := map[string]string{
+		"RJ": "Rio de Janeiro",
+		"SP": "Sao Paulo",
+		"PR": "Curitiba"}
+
+	fmt.Println("Len capitals ->", len(capitals))
+	fmt.Println("Capitals ->", capitals)
+	fmt.Println("RJ ->", capitals["RJ"])
+
 }
