9ac76d53146e9ef35fce03aad98abe33ea6fb89c
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-26 12:56:39 +0000

    Convert types

--- examples/type_conversion/main.go
+++ examples/type_conversion/main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type WishList []string
 
-func main() {
+func printSlice(slice []string) {
+	fmt.Println("Slice:", slice)
+}
 
+func printList(list WishList) {
+	fmt.Println("Lista de compras:", list)
+}
+
+func main() {
+	list := WishList{"Alface", "Atum", "Azeite"}
+	slice := []string{"Alface", "Atum", "Azeite"}
+
+	printSlice([]string(list))
+	printList(WishList(slice))
 }
