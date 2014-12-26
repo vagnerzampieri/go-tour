d8849a3277bc5558027aecf09f8699dd0a727969
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-26 11:04:05 +0000

    Populate state

--- examples/states/main.go
+++ examples/states/main.go
@@ -9,5 +9,13 @@ type State struct {
 }
 
 func main() {
+	states := make(map[string]State, 2)
+	states["GO"] = State{"Goias", 6434052, "Goiania"}
+	states["PB"] = State{"Paraiba", 3914418, "Joao Pessoa"}
 
+	fmt.Println("States ->", states)
+	fmt.Println("GO ->", states["GO"])
+	fmt.Println("GO name ->", states["GO"].name)
+	fmt.Println("GO population ->", states["GO"].population)
+	fmt.Println("GO capital ->", states["GO"].capital)
 }
