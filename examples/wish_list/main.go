0286bc3548762ff88a6be6ac6dbe68cc73130ed2
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-26 12:15:02 +0000

    Wish list

--- examples/wish_list/main.go
+++ examples/wish_list/main.go
@@ -5,5 +5,13 @@ import "fmt"
 type WishList []string
 
 func main() {
+	list := make(WishList, 6)
+	list[0] = "Alface"
+	list[1] = "Pepino"
+	list[2] = "Azeite"
+	list[3] = "Atum"
+	list[4] = "Frango"
+	list[5] = "Chocolate"
 
+	fmt.Println(list)
 }
