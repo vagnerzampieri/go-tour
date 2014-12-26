098a6575493e96c697e3119147c703eaf60d845f
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-26 12:31:49 +0000

    Create method Categorize

--- examples/wish_list/main.go
+++ examples/wish_list/main.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type WishList []string
 
+func (list WishList) Categorize() ([]string, []string, []string) {
+	vegetables, meat, other := []string{}, []string{}, []string{}
+	for _, e := range list {
+		switch e {
+		case "Alface", "Pepino":
+			vegetables = append(vegetables, e)
+		case "Atum", "Frango":
+			meat = append(meat, e)
+		default:
+			other = append(other, e)
+		}
+	}
+	return vegetables, meat, other
+}
+
 func main() {
 	list := make(WishList, 6)
 	list[0] = "Alface"
@@ -14,4 +29,9 @@ func main() {
 	list[5] = "Chocolate"
 
 	fmt.Println(list)
+
+	vegetables, meat, other := list.Categorize()
+	fmt.Println("Vegetables:", vegetables)
+	fmt.Println("Meat:", meat)
+	fmt.Println("Other:", other)
 }
