ce21cee9c0a882d52ecd7cc0592ac345b69b9e02
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-05 22:45:19 +0000

    Adding stack to print

--- examples/files/main.go
+++ examples/files/main.go
@@ -14,4 +14,10 @@ func main() {
 	file := File{"artigo.txt", 12.68, 12986, 1862, 220}
 	fmt.Println(file)
 	fmt.Printf("%s\t%.2fKB\n", file.name, file.length)
+
+	stackFile := &File{length: 7.29, name: "file.txt"}
+	fmt.Printf("before: %s\t%.2fKB\n", stackFile.name, stackFile.length)
+
+	stackFile.length = 17.47
+	fmt.Printf("after: %s\t%.2fKB\n", stackFile.name, stackFile.length)
 }
