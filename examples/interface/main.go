07ce9bfd923b3bc20bd2e6bcf59ee3dfe380a3ab
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-06 09:29:59 -0200

    Print calculations

--- examples/interface/main.go
+++ examples/interface/main.go
@@ -38,4 +38,20 @@ func main() {
 	fmt.Println("sum = Sum{10, 20} ->", sum)
 
 	fmt.Printf("%v %d\n", sum, sum.Calc())
+
+	operations := make([]Operation, 4)
+	operations[0] = Sum{30, 40}
+	operations[1] = Subtraction{30, 15}
+	operations[2] = Subtraction{10, 50}
+	operations[3] = Sum{5, 2}
+
+	accumulator := 0
+
+	for _, op := range operations {
+		value := op.Calc()
+		fmt.Printf("%v %d\n", op, value)
+		accumulator += value
+	}
+
+	fmt.Println("Valor acumulado =", accumulator)
 }
