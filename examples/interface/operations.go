72aacfaa04a5b2f2231031ea6016af9bbeea64fc
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-06 10:17:18 -0200

    Print ages

--- examples/interface/operations.go
+++ examples/interface/operations.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+type Operation interface {
+	Calc() int
+}
+
 type Age struct {
 	birthdate int
 }
@@ -17,6 +21,21 @@ func (a Age) String() string {
 	return fmt.Sprintf("Idade desde %d", a.birthdate)
 }
 
-func main() {
+func accumulate(operations []Operation) (accumulator int) {
+	for _, op := range operations {
+		value := op.Calc()
+		fmt.Printf("%v %d\n", op, value)
+		accumulator += value
+	}
 
+	return
+}
+
+func main() {
+	ages := make([]Operation, 3)
+	ages[0] = Age{1969}
+	ages[1] = Age{1977}
+	ages[2] = Age{2001}
+
+	fmt.Println("Idades acumuladas", accumulate(ages))
 }
