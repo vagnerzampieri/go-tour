98bda6e40c4b547bac5d7e55bdc1b7b068416880
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-08 10:12:09 -0200

    Adding function to create files with variadic function

--- examples/variadic_function/main.go
+++ examples/variadic_function/main.go
@@ -5,6 +5,29 @@ import (
 	"os"
 )
 
-func main() {
+func CreateFiles(dirBase string, files ...string) {
+	for _, name := range files {
+		filepath := fmt.Sprintf(
+			"%s/%s.%s", dirBase, name, "txt")
 
+		file, err := os.Create(filepath)
+
+		defer file.Close()
+
+		if err != nil {
+			fmt.Printf("Erro ao criar file %s: %v\n",
+				name, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Arquivo %s criado. \n", file.Name())
+	}
+}
+
+func main() {
+	tmp := os.TempDir()
+
+	CreateFiles(tmp)
+	CreateFiles(tmp, "test1")
+	CreateFiles(tmp, "test2", "test3", "test4")
 }
