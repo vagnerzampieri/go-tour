42435d4e6e50dd28df66033e2b8c599fb7793134
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-08 14:36:56 -0200

    Generate GenerateFibonacci is a higher order functions

--- examples/higher_order_functions/main.go
+++ examples/higher_order_functions/main.go
@@ -5,5 +5,20 @@ import (
 	"time"
 )
 
+func GenerateFibonacci(n int) func() {
+	return func() {
+		a, b := 0, 1
+
+		fib := func() int {
+			a, b = b, a+b
+			return a
+		}
+
+		for i := 0; i < n; i++ {
+			fmt.Printf("%d ", fib())
+		}
+	}
+}
+
 func main() {
 }
