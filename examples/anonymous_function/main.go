41379e7c7d028295f3113c647330c24ce5d46c6b
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-08 10:59:16 -0200

    Adding anonymous function to up words

--- examples/anonymous_function/main.go
+++ examples/anonymous_function/main.go
@@ -7,5 +7,14 @@ import (
 )
 
 func main() {
+	expr := regexp.MustCompile("\\b\\w")
 
+	upCase := func(s string) string {
+		return strings.ToUpper(s)
+	}
+
+	text := "antonio carlos jobim"
+
+	fmt.Println(upCase(text))
+	fmt.Println(expr.ReplaceAllStringFunc(text, upCase))
 }
