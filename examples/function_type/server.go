60231102347bcfd9bf72bd73fdb389f469190098
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-21 09:32:31 +0000

    Adding HTTP func

--- examples/function_type/server.go
+++ examples/function_type/server.go
@@ -7,5 +7,12 @@ import (
 )
 
 func main() {
+	http.HandleFunc(
+		"/time",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "%s",
+				time.Now().UTC().Format("2006-01-02 15:04:05"))
+		})
 
+	http.ListenAndServe(":8080", nil)
 }
