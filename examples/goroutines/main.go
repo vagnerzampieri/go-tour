c777609cc179156ddd80c9aad4fdde8588cb85e2
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-21 10:14:28 +0000

    Adding func to print time

--- examples/goroutines/main.go
+++ examples/goroutines/main.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
-func main() {
+func printTime(n int) {
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%d", n)
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("\n")
+	}
+}
 
+func main() {
+	printTime(2)
+	printTime(3)
 }
