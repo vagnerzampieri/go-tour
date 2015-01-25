0d9092d9a8bfd470d4074cb93bd6d04e1235c8ba
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-25 21:58:29 +0000

    Convert CSV file

---
+++ examples/csv/food.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	csvfile, _ := os.Open("food.csv")
+
+	defer csvfile.Close()
+
+	reader := csv.NewReader(csvfile)
+
+	rawCSVdata, _ := reader.ReadAll()
+
+	food := make(map[string][]string)
+
+	for _, each := range rawCSVdata {
+		food[each[0]] = append(food[each[0]], each[1])
+		//fmt.Printf("name: %s, type: %s\n", each[0], each[1])
+	}
+
+	for k, vs := range food {
+		var str string
+
+		str = strings.Join(vs, ", ")
+
+		fmt.Printf("<td alt=\"%s\" style=\"border-bottom: 1px solid #000000; border-left: 1px solid #0000 border-right: none; border-top: 1px solid #000000; cursor: pointer; padding-bottom: 0.04in; padding-left: 0.04in; padding-right: 0in; padding-top: 0.04in;\" title=\"%s\" width=\"14%%\">%s</td>\n", str, str, k)
+	}
+	//fmt.Println(food)
+}
